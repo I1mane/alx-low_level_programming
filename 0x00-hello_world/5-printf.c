1480db0984eb2e00c199336aece8c55c42c5d970
l1mane <elattabimane@gmail.com>
2023-02-17 15:42:07 -0800

    task 05 correct one

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,5 +7,16 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+#include <stdio.h>
+
+/**
+ * Hello from Imane's Code
+*/
+
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
 	return (0);
 }
