4b418c7d9c15ef51f17deed2baa6b15463ec75c0
l1mane <elattabimane@gmail.com>
2023-02-17 15:48:43 -0800

    task 05correct

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,22 +1,12 @@
 #include <stdio.h>
-
 /**
- * Hello from Imane's Code
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-#include <stdio.h>
-
-/**
- * Hello from Imane's Code
-*/
-
-int main(void)
-{
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
-}
 	return (0);
 }
